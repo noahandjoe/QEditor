5afadb751f26d66d132e39596a775d17801219ed
Noah <125345369+noahandjoe@users.noreply.github.com>
2023-07-09 08:35:53 +0800

    text viewer

--- qeditor.c
+++ qeditor.c
@@ -1,66 +1,645 @@
+// 特性测试宏, 是代码更具可移植性
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
-//atexit
-#include <unistd.h> 
-//read
-#include <termios.h> 
-//struct termios,tcgetattr(),tcsetattr(),ECHO,TCSAFLUSH,ICANON,ISIG,IXON,IEXTEN,ICRNL,OPOST,BRKINT,INPCK,ISTRIP,VMIN,VTIME
+// atexit
+#include <unistd.h>
+// read,write
+#include <termios.h>
+// struct termios,tcgetattr(),tcsetattr(),ECHO,TCSAFLUSH,ICANON,ISIG,IXON,IEXTEN,ICRNL,OPOST,BRKINT,INPCK,ISTRIP,VMIN,VTIME
 #include <ctype.h>
-//iscntrl
+// iscntrl
 #include <errno.h>
-//EAGAIN 
+// EAGAIN
+#include <sys/ioctl.h>
+// ioctl,TIOCGWINSZ
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+#include <stdarg.h>
 
+/******************** defines ********************/
+#define QEDITOR_VERSION "0.0.1"
+#define KILO_TAB_STOP 8
 
-/*** data***/
+#define CTRL_KEY(k) ((k)&0x1f)
 
-struct termios orig_termios;
+enum editorKey
+{
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN,
+    DEL_KEY,
+    HOME_KEY,
+    END_KEY,
+    PAGE_UP,
+    PAGE_DOWN
+};
 
+/******************** data********************/
+// 一个用于存储一行文本的数据类型
+typedef struct erow
+{
+    int size;
+    int rsize;
+    char *chars;
+    char *render;
+} erow;
 
-/*** terminal ***/
+struct editorConfig
+{
+    int cx, cy;
+    int rx;
+    int rowoff;
+    int coloff;
+    int screenrows;
+    int screencols;
+    int numrows;
+    erow *row;
+    char* filename;
+    char statusmsg[80];
+    time_t statusmsg_time;
+    struct termios orig_termios;
+};
+struct editorConfig E;
 
-//error handling
-void die(const char*s){
+/******************** terminal ********************/
+
+// error handling
+void die(const char *s)
+{
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     perror(s);
     exit(1);
 }
 
-//disable raw mode at exit
-void disableRawMode(){
-    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
+// disable raw mode at exit
+void disableRawMode()
+{
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
+        die("tcsetattr");
 }
 
-//trun off echoing
-void enableRawMode(){
-    if(tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+// trun off echoing
+void enableRawMode()
+{
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
+        die("tcgetattr");
 
     atexit(disableRawMode);
 
-    struct termios raw = orig_termios;
-    raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP ); //disable Ctrl-s/Ctrl-q
-    raw.c_oflag &= ~(OPOST); //trun off all output processing features
+    struct termios raw = E.orig_termios;
+    raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP); // disable Ctrl-s/Ctrl-q
+    raw.c_oflag &= ~(OPOST);                                  // trun off all output processing features
     raw.c_cflag |= ~(CS8);
-    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN); //trun off echoing and canonical mode and SIGINT(Ctrl-s)/SIGTSTP(Ctrl-z) signals and disable (Ctrl-v)
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN); // trun off echoing and canonical mode and SIGINT(Ctrl-s)/SIGTSTP(Ctrl-z) signals and disable (Ctrl-v)
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
-    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+        die("tcsetattr");
 }
 
-/*** init ***/
+// wait for one keypress and return it.
+int editorReadKey()
+{
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
+    {
+        // EAGAIN（表示暂时无可用数据）
+        if (nread == -1 && errno != EAGAIN)
+            die("read");
+    }
+    // 控制码的处理
+    if (c == '\x1b')
+    {
+        char seq[3];
+        if (read(STDIN_FILENO, &seq[0], 1) != 1)
+            return '\x1b';
+        if (read(STDIN_FILENO, &seq[1], 1) != 1)
+            return '\x1b';
+        // 如果第一个后续字符是 [，则可能是功能键或控制序列。
+        if (seq[0] == '[')
+        {
+            // 如果第二个后续字符是数字字符，表示功能键，需要继续读取一个后续字符
+            if (seq[1] >= '0' && seq[1] <= '9')
+            {
+                if (read(STDIN_FILENO, &seq[2], 1) != 1)
+                    return '\x1b';
+                /*如果第三个后续字符是波浪符 ~，则根据第二个后续字符确定具体的功能键，
+                并返回相应的键盘码（例如 HOME_KEY、DEL_KEY 等）*/
 
-int main() {
-    enableRawMode();
-
-    //read keypresses from the user
-    while (1){
-        char c = '\0';
-        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-        if(iscntrl(c)){ //is character a control character？
-            printf("%d\r\n", c);
-        }else{
-            printf("%d ('%c')\r\n",c,c); //start a new line
+                if (seq[2] == '~')
+                {
+                    //      \x1b[1~
+                    switch (seq[1])
+                    {
+                    case '1':
+                        return HOME_KEY;
+                    case '3':
+                        return DEL_KEY;
+                    case '4':
+                        return END_KEY;
+                    case '5':
+                        return PAGE_UP;
+                    case '6':
+                        return PAGE_DOWN;
+                    case '7':
+                        return HOME_KEY;
+                    case '8':
+                        return END_KEY;
+                    }
+                }
+            }
+            else
+            /*根据第二个后续字符确定具体的控制序列，
+            并返回相应的键盘码（例如 ARROW_UP、ARROW_DOWN 等）。*/
+            {
+                //      \x1b[H
+                switch (seq[1])
+                {
+                case 'A':
+                    return ARROW_UP;
+                case 'B':
+                    return ARROW_DOWN;
+                case 'C':
+                    return ARROW_RIGHT;
+                case 'D':
+                    return ARROW_LEFT;
+                case 'H':
+                    return HOME_KEY;
+                case 'F':
+                    return END_KEY;
+                }
+            }
         }
-        if(c == 'q') break;
+        else if (seq[0] == '0')
+        {
+            //      \x1b0H
+            switch (seq[1])
+            {
+            case 'H': //\x1b0H 表示将光标定位到屏幕的起始位置。
+                return HOME_KEY;
+            case 'F': //\x1b0F 表示将光标定位到屏幕的最后一行。
+                return END_KEY;
+            }
+        }
+        return '\x1b';
+    }
+    else
+    {
+        return c;
+    }
+}
+// fallback method for getting the window size
+int getCursorPosition(int *rows, int *cols)
+{
+    char buf[32];
+    unsigned int i = 0;
+
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+        return -1;
+
+    while (i < sizeof(buf) - 1)
+    {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1)
+            break;
+        if (buf[i] == 'R')
+            break;
+        i++;
+    }
+    buf[i] = '\0';
+
+    if (buf[0] != '\x1b' || buf[1] != '[')
+        return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+        return -1;
+
+    return 0;
+}
+
+int getWindowSize(int *rows, int *cols)
+{
+    struct winsize ws;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+    {
+        return -1;
+    }
+    else
+    {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
+int getWindowSize2(int *rows, int *cols)
+{
+    struct winsize ws;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+    {
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+            return -1;
+        return getCursorPosition(rows, cols);
+    }
+    else
+    {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
+/******************** row operations ********************/
+
+int editorRowCxToRx(erow* row, int cx){
+    int rx = 0;
+    int j;
+    for(j=0; j<cx; j++){
+        if(row->chars[j] == '\t'){
+            rx += (KILO_TAB_STOP - 1) - (rx % KILO_TAB_STOP);
+        }
+        rx++;
+    }
+    return rx;
+}
+
+void editorUpdateRow(erow* row){
+    int tabs = 0;
+    int j;
+    for(j=0; j<row->size; j++){
+        if(row->chars[j] == '\t') tabs++;
+    }
+    free(row->render);
+    row->render = malloc(row->size + tabs*(KILO_TAB_STOP-1) + 1);
+
+    int idx = 0;
+    for(j=0; j<row->size; j++){
+        if(row->chars[j] == '\t'){
+            row->render[idx++] = ' ';
+            while(idx % KILO_TAB_STOP != 0) row->render[idx++] = ' ';
+        }else{
+            row->render[idx++] = row->chars[j];
+        }
+    }
+    row->render[idx] = '\0';
+    row->rsize = idx;
+}
+
+
+void editorAppendRow(char *s, size_t len)
+{
+    E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+
+    int at = E.numrows;
+    E.row[at].size = len;
+    E.row[at].chars = malloc(len + 1);
+    memcpy(E.row[at].chars, s, len);
+    E.row[at].chars[len] = '\0';
+
+    E.row[at].rsize = 0;
+    E.row[at].render = NULL;
+    editorUpdateRow(&E.row[at]);
+    
+    E.numrows++;
+}
+
+/******************** file i/o ********************/
+void editorOpen(char *filename)
+{
+    free(E.filename);
+    E.filename = strdup(filename);
+
+    FILE *fp = fopen(filename, "r");
+    if (!fp)
+        die("fopen");
+
+    char *line = NULL;
+    size_t linecap = 0;
+    ssize_t linelen;
+    while ((linelen = getline(&line, &linecap, fp)) != -1)
+    {
+        while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
+        {
+            linelen--;
+        }
+        editorAppendRow(line, linelen);
+    }
+    free(line);
+    fclose(fp);
+}
+
+/******************** append buffer ********************/
+struct abuf
+{
+    char *b;
+    int len;
+};
+
+#define ABUF_INIT {NULL, 0};
+
+// append a string S to an abuf
+void abAppend(struct abuf *ab, const char *s, int len)
+{
+    char *new = realloc(ab->b, ab->len + len);
+
+    if (new == NULL)
+        return;
+    memcpy(&new[ab->len], s, len); // copy the string S after the end of the current data
+    ab->b = new;
+    ab->len += len;
+}
+
+void abFree(struct abuf *ab)
+{
+    free(ab->b);
+}
+
+/******************** output ********************/
+void editorscroll()
+{
+    E.rx = 0;
+    if(E.cy < E.numrows) {
+        E.rx = editorRowCxToRx(&E.row[E.cy], E.cx);
     }
 
-  return 0;
+    if (E.cy < E.rowoff)
+    {
+        E.rowoff = E.cy;
+    }
+    if (E.cy >= E.rowoff + E.screenrows)
+    {
+        E.rowoff = E.cy - E.screenrows + 1;
+    }
+    if (E.rx < E.coloff)
+    {
+        E.coloff = E.rx;
+    }
+    if (E.rx >= E.coloff + E.screencols)
+    {
+        E.coloff = E.rx - E.screencols + 1;
+    }
+}
+
+// draw a column of tildes(~) on the left hand side of the screen
+void editorDrawRows(struct abuf *ab)
+{
+    int y;
+    for (y = 0; y < E.screenrows; y++)
+    {
+        int filerow = y + E.rowoff;
+
+        if (filerow >= E.numrows)
+        {
+            // 当缓冲区为空时, 才会显示欢迎消息
+            if (E.numrows == 0 && y == E.screenrows / 3)
+            {
+                char welcome[80];
+                int welcomelen = snprintf(welcome, sizeof(welcome),
+                                          "QEditor -- version %s", QEDITOR_VERSION);
+                if (welcomelen > E.screencols)
+                    welcomelen = E.screencols;
+                int padding = (E.screencols - welcomelen) / 2;
+                if (padding)
+                {
+                    abAppend(ab, "~", 1);
+                    padding--;
+                }
+                while (padding--)
+                    abAppend(ab, " ", 1);
+                abAppend(ab, welcome, welcomelen);
+            }
+            else
+            {
+                abAppend(ab, "~", 1);
+            }
+        }
+        else
+        {
+            int len = E.row[filerow].rsize - E.coloff;
+            if (len < 0)
+                len = 0;
+            if (len > E.screencols)
+                len = E.screencols;
+            abAppend(ab, &E.row[filerow].render[E.coloff], len);
+        }
+
+        abAppend(ab, "\x1b[K", 3); // erases part of the current line
+        abAppend(ab, "\r\n", 2);
+    }
+}
+
+void editorDrawStatusBar(struct abuf* ab){
+    abAppend(ab, "\x1b[7m", 4); //反转颜色显示
+    char status[80],  rstatus[80];
+    int len = snprintf(status, sizeof(status), "%.20s - %d lines",
+        E.filename ? E.filename : "[No Name]", E.numrows);
+    
+    int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", 
+        E.cy + 1, E.numrows);
+    if(len > E.screencols) len = E.screencols;
+    abAppend(ab, status, len);
+
+    while(len < E.screencols){
+        if(E.screencols - len == rlen){
+            abAppend(ab, rstatus, rlen);
+            break;
+        }else{
+            abAppend(ab, " ", 1);
+            len++;
+        }
+    }
+    abAppend(ab, "\x1b[m", 3); //关闭反转颜色显示 默认0
+    abAppend(ab, "\r\n", 2);
+}
+
+void editorDrawMessageBar(struct abuf* ab){
+    abAppend(ab, "\x1b[K", 3);
+    int msglen = strlen(E.statusmsg);
+    if(msglen > E.screencols) msglen = E.screencols;
+    if(msglen && time(NULL) - E.statusmsg_time < 5)
+        abAppend(ab, E.statusmsg, msglen);
+}
+
+/*
+刷新屏幕显示。函数通过操作字符缓冲区 abuf 实现绘制并输出到屏幕上。
+*/
+
+void editorRefreshScreen()
+{
+    editorscroll();
+
+    struct abuf ab = ABUF_INIT;
+    /*
+    ?25l 是用于隐藏光标的控制码，其中：
+        ? 表示参数序列的开始。
+        25 是控制码的参数，表示光标的显示/隐藏。
+        l 表示将参数应用到相应的设置，这里是将参数应用到光标显示/隐藏设置。
+    */
+    abAppend(&ab, "\x1b[?25l", 6);
+    abAppend(&ab, "\x1b[H", 3);
+
+    editorDrawRows(&ab);
+    editorDrawStatusBar(&ab);
+    editorDrawMessageBar(&ab);
+
+    char buf[32];
+    /*将光标位置信息格式化为字符串，并将其添加到缓冲区 ab 中*/
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.rx - E.coloff) + 1);
+    abAppend(&ab, buf, strlen(buf));
+
+    /*添加控制码 \x1b[?25h 到缓冲区 ab 中，用于恢复显示光标*/
+    abAppend(&ab, "\x1b[?25h", 6);
+
+    write(STDOUT_FILENO, ab.b, ab.len); // buffer's contents out to standard output
+    abFree(&ab);                        // free the memory
+}
+
+void editorSetStatusMessage(const char* fmt, ...){
+    va_list ap;
+    va_start(ap, fmt);
+    vsnprintf(E.statusmsg, sizeof(E.statusmsg), fmt, ap);
+    va_end(ap);
+    E.statusmsg_time = time(NULL);
+}
+
+/******************** input ********************/
+void editorMoveCursor(int key)
+{
+    erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+    switch (key)
+    {
+    case ARROW_LEFT:
+        if (E.cx != 0)
+        {
+            E.cx--;
+        }else if(E.cy > 0){
+            E.cy--;
+            E.cx = E.row[E.cy].size;
+        }
+        break;
+    case ARROW_RIGHT:
+        if (row && E.cx < row->size)
+        {
+            E.cx++;
+        }else if(row && E.cx == row->size){
+            E.cy++;
+            E.cx = 0;
+        }
+        break;
+    case ARROW_UP:
+        if (E.cy != 0)
+        {
+            E.cy--;
+        }
+        break;
+    case ARROW_DOWN:
+        if (E.cy < E.numrows)
+        {
+            E.cy++;
+        }
+        break;
+    }
+
+    row = (E.cy >= E.numrows) ? NULL:&E.row[E.cy];
+    int rowlen = row?row->size:0;
+    if(E.cx > rowlen){
+        E.cx = rowlen;
+    }
+}
+
+void editorProcessKeypress()
+{
+    int c = editorReadKey();
+    switch (c)
+    {
+    case CTRL_KEY('q'):
+        write(STDOUT_FILENO, "\x1b[2J", 4);
+        write(STDOUT_FILENO, "\x1b[H", 3);
+
+        exit(0);
+        break;
+
+    case HOME_KEY:
+        E.cx = 0;
+        break;
+    case END_KEY:
+        if(E.cy < E.numrows)
+            E.cx = E.row[E.cy].size;
+        break;
+
+    case PAGE_UP:
+    case PAGE_DOWN:
+    {
+        if(c==PAGE_UP){
+            E.cy = E.rowoff;
+        }else if(c == PAGE_DOWN){
+            E.cy = E.rowoff + E.screenrows - 1;
+            if(E.cy > E.numrows) E.cy = E.numrows;
+        }
+        int times = E.screenrows;
+        while (times--)
+            editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+    }
+    break;
+
+    case ARROW_UP:
+    case ARROW_DOWN:
+    case ARROW_LEFT:
+    case ARROW_RIGHT:
+        editorMoveCursor(c);
+        break;
+    }
+}
+
+/*
+\x1b 是 ASCII 转义字符的表示，十进制的27, 用于引导控制码的输入
+[ 表示控制码的开始
+*/
+
+/******************** init ********************/
+
+void initEditor()
+{
+    E.cx = 0;
+    E.cy = 0;
+    E.rx = 0;
+    E.rowoff = 0;
+    E.numrows = 0;
+    E.row = NULL;
+    E.filename = NULL;
+    E.statusmsg[0] = '\0';
+    E.statusmsg_time = 0;
+
+    if (getWindowSize2(&E.screenrows, &E.screencols) == -1)
+        die("getWindowSize");
+    
+    E.screenrows -= 2;
+}
+
+int main(int argc, char *argv[])
+{
+    enableRawMode();
+    initEditor();
+    if (argc >= 2)
+    {
+        editorOpen(argv[1]);
+    }
+
+    editorSetStatusMessage("HELP: Ctrl-Q = quit");
+
+    // read keypresses from the user
+    while (1)
+    {
+        editorRefreshScreen();
+        editorProcessKeypress();
+    }
+
+    return 0;
 }
